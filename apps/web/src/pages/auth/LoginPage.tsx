14bfe294f71350840738243dca63c1dcfb6fa4ce
Markus Baumer <markus@baumer.dev>
2026-08-04 17:19:14 +0200

    Login-Screen Bild ausgetauscht

--- apps/web/src/pages/auth/LoginPage.tsx
+++ apps/web/src/pages/auth/LoginPage.tsx
@@ -6,11 +6,15 @@ import { LoginForm } from "./LoginForm";
 export const LoginPage = () => {
   useDocumentTitle(t("ui.auth.login"));
   return (
-    <div className="grid min-h-dvh bg-white md:grid-cols-2">
+    <div className="grid min-h-dvh grid-rows-[1fr_auto] max-md:[@media(min-height:780px)]:grid-rows-[556px_1fr] bg-white md:grid-cols-[min(480px,50%)_1fr] md:grid-rows-1">
       <div className="flex flex-col items-center justify-center gap-16 p-6 md:p-10">
         <div className="flex items-center gap-3 font-medium">
-          <img src={logoUrl} alt="" className="size-12" />
-          <span className="font-heading font-bold text-3xl">
+          <img
+            src={logoUrl}
+            alt=""
+            className="size-12 max-[379px]:size-10 max-[329px]:size-8 md:max-[959px]:size-10"
+          />
+          <span className="font-heading font-bold text-3xl max-[379px]:text-2xl max-[329px]:text-xl md:max-[959px]:text-2xl">
             <span className="text-sky-700">{t("common.appName.Einfach")}</span>
             <span className="text-teal-600">
               {t("common.appName.Vermieter")}
@@ -21,14 +25,12 @@ export const LoginPage = () => {
           <LoginForm />
         </div>
       </div>
-      <div className="relative hidden bg-muted md:block">
+      <div className="relative h-56 bg-muted max-md:[@media(max-height:699px)]:hidden max-md:[@media(min-height:780px)]:h-full md:h-auto">
         <img
-          src="/apartment-1024.jpg"
-          srcSet="/apartment-512.jpg 512w, /apartment-768.jpg 768w, /apartment-1024.jpg 1024w"
-          sizes="50vw"
+          src="/login-house.webp"
           alt=""
           aria-hidden="true"
-          className="absolute inset-0 size-full object-cover"
+          className="absolute inset-0 size-full object-cover object-[80%_50%]"
         />
       </div>
     </div>
